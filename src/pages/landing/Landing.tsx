8d6cb3ebbb45314451df47df8ca9638f4fa6d586
martinmsaavedra <martin@bildung.uy>
2024-05-13 15:34:35 -0300

    jl first commit

--- src/pages/landing/Landing.tsx
+++ src/pages/landing/Landing.tsx
@@ -1,11 +1,10 @@
-import { useEffect, useState } from "react";
-import type { Schema } from "../../../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
+// import type { Schema } from "../../../amplify/data/resource";
+// import { generateClient } from "aws-amplify/data";
 import { Button } from "primereact/button";
 import "primereact/resources/themes/lara-dark-cyan/theme.css";
 import "primeicons/primeicons.css";
 
-const client = generateClient<Schema>();
+// const client = generateClient<Schema>();
 
 function Landing() {
   //const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
